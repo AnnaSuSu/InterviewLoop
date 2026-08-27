689f2c48eb213bee2fe2367cd31aa81e23c9dae8
Aari <ansusu1025@gmail.com>
2026-08-27 18:25:11 +0800

    test(provider): align structured chat options

--- tests-ts/interview.test.ts
+++ tests-ts/interview.test.ts
@@ -8,6 +8,7 @@ import {
   ProfileService,
   ResumeInterviewEngine,
   type CandidateProfilePort,
+  type ChatCompleteOptions,
   type ChatMessage,
   type InterviewDependencies,
   type KnowledgeStore,
@@ -31,9 +32,9 @@ const context: RequestContext = { requestId: 'test', userId: 'user-a', signal: n
 
 class FakeAi implements TextGenerationUseCases {
   calls: ChatMessage[][] = []
-  options: Array<{ maxTokens?: number; temperature?: number } | undefined> = []
+  options: Array<ChatCompleteOptions | undefined> = []
   constructor(private readonly replies: string[]) {}
-  async complete(_context: RequestContext, messages: readonly ChatMessage[], options?: { maxTokens?: number; temperature?: number }): Promise<string> {
+  async complete(_context: RequestContext, messages: readonly ChatMessage[], options?: ChatCompleteOptions): Promise<string> {
     this.calls.push([...messages])
     this.options.push(options)
     const reply = this.replies.shift()
